460ad5cc1bb859bf5e2794fe92912201d04e04ce
herrenchek <giselle.herr@icloud.com>
2019-09-08 22:39:43 -0400

    Basic display

--- clicky-game/src/components/Main.js
+++ clicky-game/src/components/Main.js
@@ -2,11 +2,59 @@ import React from "react";
 
 function Main() {
     return (
-        <div className="main">
-            <div className="card">
-                <img className="card-img-top" width="170px" height="170px" src="./assets/images/done.jpg" alt="Kris Kardashian crying."></img>
+        <main role="main">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/done.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/tissue.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/joanne.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kim-cant-stop-crying.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kim-kardashian-crying-on-phone.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kourtney.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-crying-header.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-jenner-cries-about-bruce.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-jenner.jpg"></img>
+                        </div>
+                    </div>
+                {/* ./row */}
+                </div>
+            {/* ./container */}
             </div>
-        </div>
+        </main>
     );
 }
 
