e62d871e2ca89b466f4ebb66f77e2f87063e5710
herrenchek <giselle.herr@icloud.com>
2019-09-08 22:17:17 -0400

    Set up basic components

---
+++ clicky-game/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Made with &#9829; by <a href="https://github.com/herrenchek">herrenchek</a></p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ clicky-game/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div className="main">
+            <div className="card">
+                <img className="card-img-top" width="170px" height="170px" src="./assets/images/done.jpg" alt="Kris Kardashian crying."></img>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
