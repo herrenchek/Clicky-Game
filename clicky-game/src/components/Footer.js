2a12c31eb5a437d6916ed2fd483111dc04a9dedd
herrenchek <giselle.herr@icloud.com>
2019-09-22 22:12:54 -0400

    CSS and card component set up

--- clicky-game/src/components/Footer.js
+++ clicky-game/src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+const footerStyle = {
+    textAlign: 'center'
+};
+
 function Footer() {
     return (
-        <div className="footer">
+        <div className="footer" style={footerStyle}>
             <p>Made with &#9829; by <a href="https://github.com/herrenchek">herrenchek</a></p>
         </div>
     );
