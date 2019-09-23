b2f6842c3b4fef917572cddc87d6c47a1ddf5522
herrenchek <giselle.herr@icloud.com>
2019-09-22 21:17:44 -0400

    Header

--- clicky-game/src/components/Header.js
+++ clicky-game/src/components/Header.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Header() {
     return (
-    <div className="jumbotron">
-        <h1>Which Kardashian Crying Are You?</h1>
-        <p>Click on an image to earn points, but don't click on any more than once!</p>
-    </div>
+        <header>
+            <div className="jumbotron">
+                <h1>Which Kardashian Crying Are You?</h1>
+                <p>Click on an image to earn points, but don't click on any more than once!</p>
+            </div>
+        </header>
     );
 }
 
