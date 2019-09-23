9485516a0320fcd17fc95dc33bd2d986bb8b978e
herrenchek <giselle.herr@icloud.com>
2019-09-23 00:32:29 -0400

    Background image does not work

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,47 +3,47 @@ import React from "react";
 const images = [
     {
         id: "0",
-        image: "/assets/images/done.jpg",
+        image: "assets/images/done.jpg",
         alt: "Placeholder."
     },
     {
         id: "1",
-        image: "/assets/images/kim-cant-stop-crying.jpg",
+        image: "assets/images/kim-cant-stop-crying.jpg",
         alt: "Placeholder."
     },
     {
         id: "2",
-        image: "/assets/images/kim-kardashian-crying-on-phone.jpg",
+        image: "assets/images/kim-kardashian-crying-on-phone.jpg",
         alt: "Placeholder."
     },
     {
         id: "3",
-        image: "/assets/images/kourtney.jpg",
+        image: "assets/images/kourtney.jpg",
         alt: "Placeholder."
     },
     {
         id: "4",
-        image: "/assets/images/kris-crying-header.jpg",
+        image: "assets/images/kris-crying-header.jpg",
         alt: "Placeholder."
     },
     {
         id: "5",
-        image: "/assets/images/kris-jenner-cries-about-bruce.jpg",
+        image: "assets/images/kris-jenner-cries-about-bruce.jpg",
         alt: "Placeholder."
     },
     {
         id: "6",
-        image: "/assets/images/kris-jenner.jpg",
+        image: "assets/images/kris-jenner.jpg",
         alt: "Placeholder."
     },
     {
         id: "7",
-        image: "/assets/images/tissue.jpg",
+        image: "assets/images/tissue.jpg",
         alt: "Placeholder."
     },
     {
         id: "8",
-        image: "/assets/images/joanne.jpg",
+        image: "assets/images/joanne.jpg",
         alt: "Placeholder."
     }
 ]
