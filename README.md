e2c4aab7d82bf27dbd1c3d11f2e694f20eb87dfb
herrenchek <giselle.herr@icloud.com>
2019-09-22 22:24:20 -0400

    Fixed folder structure

---
+++ README.md
@@ -0,0 +1,10 @@
+# Clicky-Game
+A memory game created with React. This application's UI is broken up into components that change state and respond to user events.
+
+[View repository].(https://github.com/herrenchek/Clicky-Game)
+
+## Tech Used
+`HTML`
+`CSS`
+`JavaScript`
+`React`
---
+++ src/components/Main.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+function Main() {
+    return (
+        <main role="main">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/done.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/tissue.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/joanne.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kim-cant-stop-crying.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kim-kardashian-crying-on-phone.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kourtney.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-crying-header.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-jenner-cries-about-bruce.jpg"></img>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img className="card-img-top" src="./assets/images/kris-jenner.jpg"></img>
+                        </div>
+                    </div>
+                {/* ./row */}
+                </div>
+            {/* ./container */}
+            </div>
+        </main>
+    );
+}
+
+export default Main;
